85aeac0b638025795959e48bb6197ee974218117
Niraj Roy <85794375+Niraj-Roy@users.noreply.github.com>
2022-01-14 19:42:39 +0530

    Update Code.js

--- Code.js
+++ Code.js
@@ -1,1 +1,20 @@
-javascript:(function () { %C2%A0%C2%A0%C2%A0 var script =%C2%A0 document.createElement('script'); %C2%A0%C2%A0%C2%A0 script.src="//cdn.jsdelivr.net/npm/eruda"; %C2%A0%C2%A0%C2%A0 document.body.appendChild(script); %C2%A0%C2%A0%C2%A0 script.onload = function () { %C2%A0%C2%A0%C2%A0%C2%A0%C2%A0%C2%A0%C2%A0 eruda.init() %C2%A0%C2%A0%C2%A0 } })();
+javascript:(function () {     
+
+var script =  document.createElement('script');     
+
+script.src="//cdn.jsdelivr.net/npm/eruda";     
+
+document.body.appendChild(script);     
+
+script.onload = function () {         
+
+eruda.init() 
+
+    }
+
+  }
+
+ )
+
+();
+
